ac610e54916ab528e2a794b02c4164523c81cf41
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-07-14 11:34:01 +0800

    wip: Salon view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container mt-4">
     <div class="row justify-content-between">
         <div class="col-md-6 flex">
-            <button class="btn btn-sm btn-default border border-black">NEW & ACTIVE ACCNT</button>
-            <button class="btn btn-sm btn-default border border-black">EXPIRED & COMPLETED ACCNT</button>
+            <button class="btn btn-sm btn-default border btn__ff--primary">NEW & ACTIVE ACCNT</button>
+            <button class="btn btn-sm btn-default border btn__ff--primary">EXPIRED & COMPLETED ACCNT</button>
         </div>
         <div class="col-md-5">
             <div class="input-group md-form form-sm form-1 pl-0">
@@ -21,42 +21,45 @@
     <div class="row pt-4">
         <div class="col-md-12">
 
-            <table class="table">
-                <thead class="text-white bg-primary">
-                    <tr>
-                        <th scope="col" class="p-1">REF. NUMBER</th>
-                        <th scope="col" class="p-1">FIRST NAME</th>
-                        <th scope="col" class="p-1">LAST NAME</th>
-                        <th scope="col" class="p-1">PAYMENT</th>
-                        <th scope="col" class="p-1">PACKAGE</th>
-                        <th scope="col" class="p-1">DATE OF VISITS</th>
-                        <th scope="col" class="p-1">DATE VALID</th>
-                        <th scope="col" class="p-1">STATUS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="text-white bg-primary">
+                        <tr>
+                            <th scope="col" class="p-1">REF. NUMBER</th>
+                            <th scope="col" class="p-1">FIRST NAME</th>
+                            <th scope="col" class="p-1">LAST NAME</th>
+                            <th scope="col" class="p-1">PAYMENT</th>
+                            <th scope="col" class="p-1">PACKAGE</th>
+                            <th scope="col" class="p-1">DATE OF VISITS</th>
+                            <th scope="col" class="p-1">DATE VALID</th>
+                            <th scope="col" class="p-1">STATUS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <th>1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th>1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <th>1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
 
         </div>
     </div>
