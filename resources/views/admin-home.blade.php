2b64021bbb7b78b32b5a92ff1fbb73c9efb4cd82
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-07-16 10:16:04 +0800

    Creation of admin branch list

--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-6 flex">
             <button class="btn btn-default border border-dark text-center w-100 bg-light">
                 <span class="float-left">+</span>
-                <span>Add Branch</span>
+                <span class="text-bold font-15px">Add Branch</span>
             </button>
         </div>
 
@@ -62,56 +62,53 @@
 
     <div class="row">
         <div class="col-md-6 admin-branches">
-            {{-- @for ($i = 5; $i < $count; $i++)
+            @for($i = 0; $i < 5; $i++)
+                <div class="card mb-3 branch">
+                    <div class="card-header d-flex justify-content-between align-items-center cursor-pointer @if( $i === 0 ) text-white bg-primary @else text-dark @endif"
+                        title="Click to Activate.">
+                        <span class="d-inline-block text-bold"> > </span>
+                        <div class="w-90 d-flex justify-content-between">
+                            <div>
+                                <strong class="d-block mb-0">Caniogan, Pasig</strong>
+                                <span class="mb-0">Branch ID: 000000000</span>
+                            </div>
 
-            @endfor --}}
-            <div class="card mb-3">
-                <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-                    <span class="d-inline-block"> > </span>
-                    <div class="w-90 d-flex justify-content-between">
-                        <div>
-                            <strong class="d-block mb-0">Caniogan, Pasig</strong>
-                            <span class="mb-0">Branch ID: 000000000</span>
+                            <div>
+                                <p class="mb-0">Status: Active</p>
+                                <p class="mb-0">Date Opened: 9/1/2020</p>
+                            </div>
                         </div>
 
-                        <div>
-                            <p class="mb-0">Status: Active</p>
-                            <p class="mb-0">Date Opened: 9/1/2020</p>
+                    </div>
+                    <div class="card-body w-90 align-self-end branch__info">
+                        <div class="d-flex align-items-base branch__address">
+                            <img src="{{ asset('svg/icons/branch_location_icon.svg') }}"
+                                alt="Branch Location Icon" width="20" height="20" />
+                            <address class="w-80">213 Gemini St. Rainbow Village 2, UPS 5, Sucat Rd. San Isidro, Metro
+                                Manila - Paranaque
+                                City 1700, NCR</address>
+                        </div>
+                        <div class="d-flex branch__users">
+                            <img src="{{ asset('svg/icons/branch_user_icon.svg') }}"
+                                alt="Branch User Icon" width="20" height="20" />
+
+                            <div class="user_info">
+                                <p class="mb-0">John Doe</p>
+                                <p class="mb-0">Branch Manager</p>
+                                <p class="mb-0">johndoe@gmail.com</p>
+                            </div>
+
+                            <div class="switcher">
+                                <label class="switch">
+                                    <input type="checkbox">
+                                    <span class="slider round"></span>
+                                </label>
+                            </div>
+
                         </div>
                     </div>
-
                 </div>
-                <div class="card-body w-90 align-self-end">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-
-            <div class="card mb-3">
-                <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-                    <span class="d-inline-block"> > </span>
-                    <div class="w-90 d-flex justify-content-between">
-                        <div>
-                            <strong class="d-block mb-0">Caniogan, Pasig</strong>
-                            <span class="mb-0">Branch ID: 000000000</span>
-                        </div>
-
-                        <div>
-                            <p class="mb-0">Status: Active</p>
-                            <p class="mb-0">Date Opened: 9/1/2020</p>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="card-body w-90 align-self-end">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-
-
+            @endfor
         </div>
 
         <div class="col-md-6">
