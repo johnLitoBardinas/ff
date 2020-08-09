730f5028cb93115cff5db6657b61de1b31b1c70f
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-08-09 08:48:02 +0800

    wip: Refactor AdminDashboard

--- app/Http/Controllers/AdminDashboard.php
+++ app/Http/Controllers/AdminDashboard.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AdminDashboard extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         return view('admin-home')->with([
             'logo' => 'svg/fixandfree.co_logo_horizontal.svg',
@@ -14,12 +12,3 @@ class AdminDashboard extends Controller
         ]);
     }
 }
-
-/**
- * This Controller will facilitate the Request of an Admin,
- * Contains the following ACTIONS:
- *
- * 1. CRU D(diactivate) Delete ? Need to clarify this shit - BRANCH
- * 1.1 Searching via Branch Name and Branch ID/Code
- * 2. USers for a specific Branch. CRU (D) Deactivate -> inactive database.
- */
