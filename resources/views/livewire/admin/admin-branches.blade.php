a8b0db3166be2ec4de087fe00a0b790982d5d67d
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-09-02 23:02:55 +0800

    done: Implement the Branches -> Branch design with icons

--- resources/views/livewire/admin/admin-branches.blade.php
+++ resources/views/livewire/admin/admin-branches.blade.php
@@ -4,7 +4,12 @@
             <div class="card-header d-flex justify-content-between align-items-center cursor-pointer"
                 x-bind:class="[ {{ $branch->branch_id }} === {{$activeBranchId}} ? 'text-white bg-primary' : 'text-dark']"
                 title="Click to Activate." wire:click="changeBranch({{$branch->branch_id}})">
-                <span class="d-inline-block text-bold"> > </span>
+                <span class="d-inline-block text-bold"> &#62;</span>
+
+                <div class="w-10 d-flex">
+                <x-icon type="{{$branch->branch_type}}" active="{{ $branch->branch_id === $activeBranchId }}"/>
+                </div>
+
                 <div class="w-90 d-flex justify-content-between">
                     <div>
                         <strong class="d-block mb-0">{{ $branch->branch_name }}</strong>
@@ -18,44 +23,15 @@
                         </p>
                     </div>
                 </div>
-
             </div>
+            {{-- ./card-header --}}
             <div class="card-body w-90 align-self-end branch__info">
-                <div class="d-flex align-items-base branch__address">
-                    <span class="icon icon__location--black"></span>
-                    <address class="w-80">{{ $branch->branch_address }}</address>
-                </div>
-                @forelse($branch->users as $user)
-                    <div class="d-flex branch__users" x-data="{ userStatus: '{{ $user->user_status }}' }"
-                        :class="{ 'opacity-point5' : userStatus === 'inactive' }">
-                        <span class="icon icon__account--black"></span>
-
-                        <div class="user_info w-75">
-                            <p class="mb-0"><b>Name:</b>
-                                {{ sprintf('%s %s', $user->first_name, $user->last_name) }}
-                            </p>
-                            <p class="mb-0"><b>Type:</b> {{ $user->role->name }}</p>
-                            <p class="mb-0"><b>Email:</b> {{ $user->email }}</p>
-                        </div>
-
-                        <div class="switcher">
-                            <label class="switch">
-                                <input type="checkbox" wire:click="toggleUserStatus({{ $user->user_id }})"
-                                    x-bind:checked="userStatus === 'active'">
-                                <span class="slider round"></span>
-                            </label>
-                        </div>
-
-                    </div>
-                    <br>
-                @empty
-                    <h6>No User yet..</h6>
-                @endforelse
-
+                Card Body
             </div>
+            {{-- ./ card-body --}}
         </div>
     @empty
-        <h2>Empty Branch.</h2>
+        <h2>Empty Branch...</h2>
     @endforelse
 </div>
 
