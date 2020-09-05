7be9498393bc881267c963855ffcc4c5771f1503
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-09-05 09:19:52 +0800

    wip: Update the create_package migration

--- database/migrations/2020_07_12_065427_create_package_table.php
+++ database/migrations/2020_07_12_065427_create_package_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PackageStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,9 +17,16 @@ class CreatePackageTable extends Migration
         Schema::create('package', function (Blueprint $table) {
             $table->bigIncrements('package_id');
             $table->string('package_name')->unique();
-            $table->string('package_description');
             $table->decimal('package_price');
+            $table->enum('package_status', PackageStatus::getValues())->default(PackageStatus::INACTIVE);
+            $table->tinyInteger('salon_no_of_visits')->unsigned();
+            $table->tinyInteger('salon_days_valid_count')->unsigned();
+            $table->tinyInteger('gym_no_of_visits')->unsigned()->default(0); // if 0 then it is only days
+            $table->tinyInteger('gym_days_valid_count')->unsigned();
+            $table->tinyInteger('spa_no_of_visits')->unsigned();
+            $table->tinyInteger('spa_days_valid_count');
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::table('package', function (Blueprint $table)
--- database/seeds/PackageSeeder.php
+++ database/seeds/PackageSeeder.php
@@ -11,23 +11,22 @@ class PackageSeeder extends Seeder
     public function run()
     {
         // factory(Package::class, 5)->create();
-        Package::create([
-            'package_name' => 'plan 1500',
-            'package_description' => 'nails, spa, etc.',
-            'package_price' => 1500.00,
-        ]);
+        // Package::create([
+        //     'package_name' => 'plan 1500',
+        //     'package_price' => 1500.00,
+        // ]);
 
-        Package::create([
-            'package_name' => 'plan 1800',
-            'package_description' => 'nails, spa, dryer, etc.',
-            'package_price' => 1800.00,
-        ]);
+        // Package::create([
+        //     'package_name' => 'plan 1800',
+        //     'package_description' => 'nails, spa, dryer, etc.',
+        //     'package_price' => 1800.00,
+        // ]);
 
-        Package::create([
-            'package_name' => 'plan 2000',
-            'package_description' => 'all',
-            'package_price' => 2000.00,
-        ]);
+        // Package::create([
+        //     'package_name' => 'plan 2000',
+        //     'package_description' => 'all',
+        //     'package_price' => 2000.00,
+        // ]);
 
     }
 }
