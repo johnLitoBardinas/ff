5691cdff46dda59d0ce0909802a053b91563de18
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-09-14 00:06:02 +0800

    wip: Initial Implementation for the salon table

--- resources/views/livewire/salon/salon-table.blade.php
+++ resources/views/livewire/salon/salon-table.blade.php
@@ -1,7 +1,14 @@
 <div class="row mt-4">
     <div class="col-md-12">
         <div class="table-responsive">
-            <table class="table table-hover admin-table">
+            <table class="table table-hover admin-table" style="width:100%;" x-data="{isOpen:false}">
+                <colgroup>
+                    <col span="1" style="width: 10%;">
+                    <col span="1" style="width: 15%;">
+                    <col span="1" style="width:10%;">
+                    <col span="1" style="width: 60%;">
+                    <col span="1" style="width: 5%;">
+                </colgroup>
                 <thead class="text-white bg-primary">
                     <tr>
                         <th scope="col">
@@ -16,112 +23,106 @@
                         <th scope="col">
                             PACKAGE
                         </th>
-                        <th scope="col">
-                            TYPE
-                        </th>
-                        <th scope="col">
-                            PRICE
-                        </th>
-                        <th scope="col">
-                            DATE OF VISITS
-                            <small><em>(Month-Day-Year)</em></small>
-                        </th>
-                        <th scope="col">
-                            DATE VALID
-                        </th>
-                        <th scope="col">
-                            STATUS
-                        </th>
                         <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
-                    @forelse ($customerPackageVisitsInfo as $row)
-                        <tr>
-                            <td>{{strtoupper($row->reference_no)}}</td>
-                            <td>{{ucwords($row->customer->first_name). ' ' .ucwords($row->customer->last_name)}}</td>
-                            <td>{{strtoupper($row->payment_type)}}</td>
-                            <td>{{strtoupper($row->package->package_name)}}</td>
-                            <td>
-                                <div class="d-flex flex-column">
-                                    @if (session('userAccessType') === 'salon')
-                                        <h6 class="mt-2">{{ucfirst(session('userAccessType'))}}</h6>
-                                        <h6 class="mt-2">Gym</h6>
-                                        <h6 class="mt-2">Spa</h6>
-                                    @elseif(session('userAccessType') === 'gym')
-                                        <h6 class="mt-2">{{ucfirst(session('userAccessType'))}}</h6>
-                                        <h6 class="mt-2">Salon</h6>
-                                        <h6 class="mt-2">Spa</h6>
-                                    @elseif(session('userAccessType') === 'spa')
-                                        <h6 class="mt-2">{{ucfirst(session('userAccessType'))}}</h6>
-                                        <h6 class="mt-2">Salon</h6>
-                                        <h6 class="mt-2">Gym</h6>
-                                    @endif
-                                </div>
-                            </td>
-                            <td>
-                                <div class="d-flex flex-column">
-                                    <h6 class="mt-2">{{number_format($row->package->package_price)}}</h6>
-                                    <h6 class="mt-2 text-line-through">Free</h6>
-                                    <h6 class="mt-2 text-line-through">Free</h6>
-                                </div>
-                            </td>
-                            <td>
+                    <tr>
+                        <td>000000000000</td>
+                        <td>Francisco, Stephen Reon</td>
+                        <td>Paymaya</td>
+                        <td>
+                            <div class="table-responsive table-hover cursor-pointer">
+                                <table class="table">
+                                    <thead style="white-space:nowrap;">
+                                        <tr>
+                                            <th class="text-primary font-weight-bold">NAME</th>
+                                            <th class="text-primary font-weight-bold">TYPE</th>
+                                            <th class="text-primary font-weight-bold">PRICE</th>
+                                            <th class="text-primary font-weight-bold">VISITS</th>
+                                            <th class="text-primary font-weight-bold">VALID UNTIL</th>
+                                            <th class="text-primary font-weight-bold">STATUS</th>
+                                        </tr>
+                                    </thead>
+                                    <template x-if="isOpen">
+                                    <tbody style="white-space:nowrap;">
+                                        <tr>
+                                            <td>GROOM</td>
+                                            <td>SALON</td>
+                                            <td>1,800</td>
+                                            <td>
+                                                <div class="w-100 d-flex justify-content-between">
+                                                    @for ($i = 0; $i < 4; $i++)
+                                                        <div class="w-auto d-flex flex-column mr-1">
+                                                            <button class="btn btn-sm btn-default border btn__ff--primary active" disabled>
+                                                                {{-- {{date('n-j-Y', strtotime($customerVisits[$i]['date']))}} --}}
+                                                                {{date('n-j-Y', strtotime(now()))}}
+                                                            </button>
+                                                        </div>
+                                                    @endfor
+                                                </div>
+                                            </td>
+                                            <td>Dec. 6, 2020</td>
+                                            <td>ACTIVE</td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>GYM</td>
+                                            <td class="text-line-through">Free</td>
+                                            <td>
+                                                <div class="w-100 d-flex justify-content-start">
+                                                    @for ($i = 0; $i < 1; $i++)
+                                                        <div class="w-auto d-flex flex-column mr-1">
+                                                            <button class="btn btn-sm btn-default border btn__ff--primary active" disabled>
+                                                                {{-- {{date('n-j-Y', strtotime($customerVisits[$i]['date']))}} --}}
+                                                                {{date('n-j-Y', strtotime(now()))}}
+                                                            </button>
+                                                        </div>
+                                                    @endfor
+                                                </div>
+                                            </td>
+                                            <td>Sept. 18, 2020</td>
+                                            <td>ACTIVE</td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>SPA</td>
+                                            <td class="text-line-through">Free</td>
+                                            <td>
+                                                <div class="w-100 d-flex justify-content-start">
+                                                    @for ($i = 0; $i < 1; $i++)
+                                                        <div class="w-auto d-flex flex-column mr-1">
+                                                            <button class="btn btn-sm btn-default border btn__ff--primary active" disabled>
+                                                                {{-- {{date('n-j-Y', strtotime($customerVisits[$i]['date']))}} --}}
+                                                                {{date('n-j-Y', strtotime(now()))}}
+                                                            </button>
+                                                        </div>
+                                                    @endfor
+                                                </div>
+                                            </td>
+                                            <td>Sept. 18, 2020</td>
+                                            <td>ACTIVE</td>
+                                        </tr>
+                                    </tbody>
+                                </template>
+                                </table>
+                            </div>
+                        </td>
+                        <td>
+                            <a
+                            href="javascript:void(0);"
+                            class="btn btn-sm btn-default btn-icon btn-icon__open-row cursor-pointer d-none"
+                            :class="{ 'd-flex' : isOpen === false }"
+                            title="View More Details"
+                            x-on:click="isOpen = true"></a>
 
-                                {{-- Paid Package Plan --}}
-                                <div class="w-100 d-flex justify-content-start">
-                                    @for ($i = 0; $i < $row->package->salon_no_of_visits; $i++)
-                                        <div class="w-auto d-flex flex-column mr-1">
-                                            <button class="btn btn-sm btn-default border btn__ff--primary active" disabled>
-                                                {{-- {{date('n-j-Y', strtotime($customerVisits[$i]['date']))}} --}}
-                                                {{date('n-j-Y', strtotime(now()))}}
-                                            </button>
-                                        </div>
-                                    @endfor
-                                </div>
-
-                                {{-- Freebies Package --}}
-                                <div class="w-100 justify-content-start mt-2 check">
-                                    Test 2
-                                </div>
-
-                                {{-- Freebies Package --}}
-                                <div class="w-100 justify-content-start mt-2 check">
-                                    Test 3
-                                </div>
-
-                            </td>
-                            <td>
-                                <?php $packageRowType = session('userAccessType') . '_package_end'; ?>
-
-                                <div class="d-flex flex-column">
-                                    <h6 class="mt-2">{{date('M. d, Y', strtotime($row->$packageRowType))}}</h6>
-                                    <h6 class="mt-2">{{date('M. d, Y', strtotime($row->$packageRowType))}}</h6>
-                                    <h6 class="mt-2">{{date('M. d, Y', strtotime($row->$packageRowType))}}</h6>
-                                </div>
-
-                            </td>
-                            <td>
-                                {{-- <span class="package-status package-status--active">{{ strtoupper($row->customer_package_status) }}</span> --}}
-                                {{-- <span class="package-status package-status--active">Active</span> --}}
-                                <div class="d-flex flex-column">
-                                    <h6 class="package-status package-status--active">Active</h6>
-                                    <h6 class="package-status package-status--active">Active</h6>
-                                    <h6 class="package-status package-status--active">Active</h6>
-                                </div>
-                            </td>
-                            <td>
-                                <a href="javascript:void(0);" class="btn btn-sm btn-default btn-icon btn-icon__open-row cursor-pointer" :class="{ 'd-flex': isOpen === false }" title="View More Details" x-on:click="alert('Test 1?')"></a>
-                                <a href="javascript:void(0);" class="btn btn-sm btn-default btn-icon btn-icon__close-row  cursor-pointer" :class="{ 'd-flex': isOpen === true }" title="View Less Details" x-on:click="alert('Test 2?')"></a>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="9">
-                                <h6>No Records Found!!!</h6>
-                            </td>
-                        </tr>
-                    @endforelse
+                            <a
+                            href="javascript:void(0);"
+                            class="btn btn-sm btn-default btn-icon btn-icon__close-row  cursor-pointer d-none"
+                            :class="{ 'd-flex': isOpen === true }"
+                            title="View Less Details" x-on:click="isOpen = false"></a>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
