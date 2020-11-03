7525298b80275e07857400921d53bb0f9f8c39e4
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-11-03 23:06:04 +0800

    done: Implementation of GymVisitation@store api endpoint

--- app/Http/Controllers/Api/GymVisitationController.php
+++ app/Http/Controllers/Api/GymVisitationController.php
@@ -26,19 +26,29 @@ class GymVisitationController extends ApiController
     {
         $todayCustomerPackageGymVisitation = AppGymVisitation::where('customer_package_id', $request->customer_package_id)->with('branch', 'user')->whereDate('date', Carbon::today())->get()->last();
 
-        if (is_null($todayCustomerPackageGymVisitation) && $request->input('visitation_type') === GymVisitationType::OUT) {
-            return $this->errorResponse('Unable to Out the customer package', 422);
+        if (is_null($todayCustomerPackageGymVisitation)) {
+            if (request('visitation_type') === GymVisitationType::IN) {
+                $gymVisitation = AppGymVisitation::create($request->all());
+                return $this->showOne($gymVisitation, 201);
+            }
+
+            return $this->errorResponse('Today customer package is not yet visited.', 422);
         }
 
-        if (! is_null($todayCustomerPackageGymVisitation) && (int) $todayCustomerPackageGymVisitation->user_id !== (int) $request->input('user_id') && $todayCustomerPackageGymVisitation->visitation_type === GymVisitationType::IN) {
-            $this->errorResponse("The customer package currently: ({$todayCustomerPackageGymVisitation->visitation_type}) branch: ({$todayCustomerPackageGymVisitation->branch->branch_name}) log by user: ({$todayCustomerPackageGymVisitation->user->first_name}, {$todayCustomerPackageGymVisitation->user->last_name})", 401);
+        if ($todayCustomerPackageGymVisitation->branch_id === (int) request('branch_id')) {
+            if ($todayCustomerPackageGymVisitation->visitation_type !== request('visitation_type')) {
+                $gymVisitation = AppGymVisitation::create($request->all());
+                return $this->showOne($gymVisitation, 201);
+            }
+
+            return $this->errorResponse("Customer Gym Package already {$todayCustomerPackageGymVisitation->visitation_type}", 422);
         }
 
-        if (! is_null($todayCustomerPackageGymVisitation) && $request->input('visitation_type') === $todayCustomerPackageGymVisitation->visitation_type) {
-            return $this->errorResponse("Unable to {$todayCustomerPackageGymVisitation->visitation_type} the customer package", 422);
+        if ($todayCustomerPackageGymVisitation->visitation_type === GymVisitationType::OUT && request('visitation_type') === GymVisitationType::IN) {
+            $gymVisitation = AppGymVisitation::create($request->all());
+            return $this->showOne($gymVisitation, 201);
         }
 
-        $gymVisitation = AppGymVisitation::create($request->all());
-        return $this->showOne($gymVisitation, 201);
+        return $this->errorResponse("The customer package currently: ({$todayCustomerPackageGymVisitation->visitation_type}) branch: ({$todayCustomerPackageGymVisitation->branch->branch_name}) log by user: ({$todayCustomerPackageGymVisitation->user->first_name}, {$todayCustomerPackageGymVisitation->user->last_name})", 401);
     }
 }
