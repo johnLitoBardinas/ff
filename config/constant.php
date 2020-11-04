54eb023bb6e7cbf58b79ede41e77a8a60a3b0be1
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-11-04 21:42:34 +0800

    Change the main branch name to the F&F.Co Main Office

--- config/constant.php
+++ config/constant.php
@@ -12,7 +12,7 @@ return [
     */
     'default_user_password' => env('DEFAULT_USER_PASSWORD'), // for security hidden to the environment variable
     'super_admin_email' => env('SUPER_ADMIN_EMAIL'),
-    'main_branch' => 'Main Branch',
+    'main_branch' => 'F&F.Co Main Office',
     'fnf_co_logo' => 'svg/fandf.co_horizontal.svg',
     'fnf_salon_logo' => 'svg/fixandfree.salon_logo_horizontal.svg', // logo for the salon account type
     'fnf_gym_logo' => 'svg/fitandfree.gym_logo_horizontal.svg', // logo for the gym account type
